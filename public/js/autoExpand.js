95c6e387b4b8995812bd6e938a6789701c744343
tontonsat <hubert.pfersdorff@gmail.com>
2019-04-12 18:04:01 +0200

    new navigation

--- public/js/autoExpand.js
+++ public/js/autoExpand.js
@@ -1,13 +1,13 @@
-$.fn.textWidth = function(text, font) {
-    
+$.fn.textWidth = function (text, font) {
+
     if (!$.fn.textWidth.fakeEl) $.fn.textWidth.fakeEl = $('<span>').hide().appendTo(document.body);
-    
+
     $.fn.textWidth.fakeEl.text(text || this.val() || this.text() || this.attr('placeholder')).css('font', font || this.css('font'));
-    
+
     return $.fn.textWidth.fakeEl.width();
 };
 
-$('.width-dynamic').on('input', function() {
+$('.width-dynamic').on('input', function () {
     var inputWidth = $(this).textWidth();
     $(this).css({
         width: inputWidth
